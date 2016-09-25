a695fd0e916be1f2c0ca85d9d5764a58ca46e644
daliMac <daliMac@192.168.0.11>
2016-09-25 16:12:23 +0200

    Add new SeeYouSoon and loveYou methods

--- Proj1/src/com/gittest/HelloWorld.java
+++ Proj1/src/com/gittest/HelloWorld.java
@@ -4,4 +4,13 @@ public class HelloWorld {
 	public void sayHello() {
 		System.out.println("Hello the world1");
 	}
+	
+	public void saySeeYouSoon() {
+		System.out.println("See you soon guys ;)");
+	}
+	
+	public void sayLoveYouBaby() {
+		System.out.println("I do love you baby");
+	}
+	
 }
